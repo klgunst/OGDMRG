0f72c7a6eb5692817d45a0106514deb365c4adae
Klaas Gunst <klaasgunst@hotmail.com>
2019-11-18 15:01:56 +0100

    Added Ising

--- ogdmrg.py
+++ ogdmrg.py
@@ -9,25 +9,31 @@ class OGDMRG:
         E: The current energy per site
         Etot: The current total energy of the system
     """
-    def __init__(self, NN_interaction=None, multipl=2):
+    def __init__(self, NN_interaction='Heisenberg', multipl=2):
         """Initializes the OGDMRG object.
 
         Args:
-            NN_interaction: The nearest neighbour interaction. If None, a
-            Heisenberg interaction is assumed.
+            NN_interaction: The nearest neighbour interaction.
+
+            This can be set to "Heisenberg" or "Ising" for Heisenberg or Ising
+            interactions respectively.
+
+            This can also be set to a tensor representing the NN interaction.
 
             For more information how the passed NN interaction should be
             structured, see the HeisenbergInteraction function.
 
-            multipl: If NN_interaction is not None, this argument is ignored.
-            If NN_interaction is None, this argument specifies the multiplicity
-            of each spin for the Heisenberg interaction.
+            multipl: If NN_interaction is not "Heisenberg" or "Ising", this
+            argument is ignored, else this argument specifies the multiplicity
+            of each spin for the given interaction.
 
             E.g. For `multipl = 2`, we work with spin-1/2.
                  For `multipl = 3`, we work with spin-1.
         """
-        if NN_interaction is None:
+        if NN_interaction == 'Heisenberg':
             self.NN_interaction = OGDMRG.HeisenbergInteraction(multipl)
+        elif NN_interaction == 'Ising':
+            self.NN_interaction = OGDMRG.IsingInteraction(multipl)
         else:
             self.NN_interaction = NN_interaction
 
@@ -58,10 +64,22 @@ class OGDMRG:
         Internally it is stored as a deque of a certain length. The current
         A-tensor is the first element of the deque, previous A-tensors are the
         other elements of the deque.
-
-        NOTE: The deque is usefull for later when guage fixing works.
         """
         return self._A_deque[0]
+
+    @property
+    def s(self):
+        return self._s_deque[0]
+
+    @s.setter
+    def s(self, s):
+        from collections import deque
+        # A deque is also initialized so to keep track of previous A's
+        try:
+            self._s_deque.appendleft(s)
+        except AttributeError:
+            # The deque is not initialized yet
+            self._s_deque = deque(s, maxlen=3)
 
     @A.setter
     def A(self, A):
@@ -74,15 +92,22 @@ class OGDMRG:
             self._A_deque = deque(A, maxlen=3)
 
     @property
-    def guageDiff(self):
-        """For later once the guage fixing works...
+    def A_diff(self):
+        """The difference between the current A and the one two iterations ago.
         """
-        raise ValueError
+        A1 = self._A_deque[0].reshape(-1, self._A_deque[0].shape[-1]) * \
+            self._s_deque[0]
+        A2 = self._A_deque[-1].reshape(-1, self._A_deque[-1].shape[-1]) * \
+            self._s_deque[-1]
+        # print(self._s_deque[0] - self._s_deque[-1])
+        # print(self._s_deque[0])
+        # print(A1.T @ A2 - np.diag(self._s_deque[0] * self._s_deque[-1]))
+        s_diag = np.diag(self._s_deque[0] * self._s_deque[-1])
+        return np.linalg.norm(A1.T @ A2 - s_diag)
 
     def S_operators(multipl=2):
         """Returns the S+, S-, and Sz operators in for a spin.
-
-        The operators are represented in the Sz basis: (-j, -j + 1, ..., j)
+The operators are represented in the Sz basis: (-j, -j + 1, ..., j)
 
         Args:
             multipl: defines which multiplicity the total spin of the site has.
@@ -109,6 +134,24 @@ class OGDMRG:
         """
         Sp, Sm, Sz = OGDMRG.S_operators(multipl)
         H = 0.5 * (np.kron(Sp, Sm) + np.kron(Sm, Sp)) + np.kron(Sz, Sz)
+        return H.reshape((multipl,) * 4)
+
+    def IsingInteraction(multipl=2):
+        """Returns Ising interaction between two sites.
+
+        This is given by:
+            1/2 * (S_1^+  + S_1^- + S_2^+ + S_2^-) + S_1^z S_2^z
+
+        Interaction is given in a dense matrix:
+            Σ H_{1', 2', 1, 2} |1'〉|2'〉〈1|〈2|
+        """
+        Sp, Sm, Sz = OGDMRG.S_operators(multipl)
+        unity = np.eye(Sp.shape[0])
+        H = 0.5 * (
+            np.kron(Sp, unity) + np.kron(Sm, unity) +
+            np.kron(unity, Sp) + np.kron(unity, Sm)
+        ) + 3.9 * np.kron(Sz, Sz)
+
         return H.reshape((multipl,) * 4)
 
     def Heff(self, x):
@@ -146,18 +189,20 @@ class OGDMRG:
         new_sval = np.concatenate(
             ([0], np.where(svd_diff > tol)[0] + 1, [len(s)])
         )
-
         # Truncating renormalized basis
         #
         # The kept basis states can be larger than te one specified by the
         # user, we just try not to cut up degenerate singular values.
         lastmultiplet = -1 if new_sval[-1] < D else np.argmax(new_sval >= D)
+
+        # If adding the full multiplet makes the bond dimension too large,
+        # discard the last multiplet
+        if new_sval[lastmultiplet] > 1.2 * D:
+            lastmultiplet -= 1
         D = new_sval[lastmultiplet]
         u = u[:, :D]
 
         # Trying to fix the guage
-        #
-        # TODO: Does not seem to work yet?
         for begin, end in zip(new_sval[:lastmultiplet], new_sval[1:]):
             U = qr(u[:, begin:end].T, mode='r').T
 
@@ -167,6 +212,7 @@ class OGDMRG:
             u[:, begin:end] = U
 
         self.A = u.reshape((self.M, self.p, -1))
+        self.s = s[:D]
         return s[D:] @ s[D:]
 
     def update_Heff(self):
@@ -199,25 +245,29 @@ class OGDMRG:
         """
         from scipy.sparse.linalg import eigsh, LinearOperator
 
-        for i in range(0, max_iter, 2):
-            # even/odd chain length
-            for j in range(2):
-                H = LinearOperator(((self.M * self.p) ** 2,) * 2,
-                                   matvec=lambda x: self.Heff(x))
-                # Diagonalize
-                w, v = eigsh(H, k=1)
-                # Renormalize the basis
-                trunc = self.renormalize_basis(v[:, 0], D)
-                # Update the effective Hamiltonian
-                self.update_Heff()
+        for i in range(max_iter):
+            H = LinearOperator(((self.M * self.p) ** 2,) * 2,
+                               matvec=lambda x: self.Heff(x))
+            # Diagonalize
+            w, v = eigsh(H, k=1, which='SA')
+            # Renormalize the basis
+            trunc = self.renormalize_basis(v[:, 0], D)
+            # Update the effective Hamiltonian
+            self.update_Heff()
 
             # Energy difference between this and the previous even-length chain
-            E = (w[0] - self.Etot) / 4
+            E = (w[0] - self.Etot) / 2
             ΔE, self.E, self.Etot = self.E - E, E, w[0]
 
-            if verbosity >= 2:
+            if verbosity >= 2 and i > 10:
                 print(f"it {i}:\tM: {self.M},\tE: {self.E:.12f},\t"
-                      f"ΔE: {ΔE:.3g},\ttrunc: {trunc:.3g}")
+                      f"ΔE: {ΔE:.3g},\ttrunc: {trunc:.3g},\t"
+                      f"A_diff {self.A_diff}"
+                      )
+            else:
+                print(f"it {i}:\tM: {self.M},\tE: {self.E:.12f},\t"
+                      f"ΔE: {ΔE:.3g},\ttrunc: {trunc:.3g},\t"
+                      )
         if verbosity >= 1:
             print(f"M: {self.M},\tE: {self.E:.12f},\t"
                   f"ΔE: {ΔE:.3g},\ttrunc: {trunc:.3g}")
@@ -232,6 +282,7 @@ if __name__ == '__main__':
     else:
         D = [16]
 
+    # ogdmrg = OGDMRG(NN_interaction="Ising")
     ogdmrg = OGDMRG()
     for d in D:
-        ogdmrg.kernel(D=d)
+        ogdmrg.kernel(D=d, max_iter=1000)
